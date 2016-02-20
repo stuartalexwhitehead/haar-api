0c811e72aad309e8474ccf0c989a132823c1136f
Stuart Whitehead <hello@stuartwhitehead.co.uk>
2016-02-20 18:21:51 +0000

    index: setting MONGO_HOST env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const haar = require('haar-engine');
 
+process.env.MONGO_HOST = process.env.HAAR_MONGO_1_PORT_27017_TCP_ADDR;
+
 haar.init();
 haar.app.get('/', (req, res) => {
   res.json('Haar API');
