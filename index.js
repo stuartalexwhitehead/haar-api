7b256c542ae5e7400d03ceed118f2af90900993b
Stuart Whitehead <hello@stuartwhitehead.co.uk>
2016-02-20 19:14:44 +0000

    index: updating env var

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const haar = require('haar-engine');
 
-process.env.MONGO_HOST = process.env.HAAR_MONGO_1_PORT_27017_TCP_ADDR;
+process.env.MONGO_HOST = process.env.HAAR_MONGO_PORT_27017_TCP_ADDR;
 
 haar.init();
 haar.app.get('/', (req, res) => {
